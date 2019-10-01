1b1c6e2b774bb33b9e25082f9f59e0b0717af0db
kefirzhang <8390671@qq.com>
2019-10-01 15:19:46 +0800

    MyStack

--- leetcode/python/p225_MyStack.py
+++ leetcode/python/p225_MyStack.py
@@ -1,29 +1,45 @@
+import queue
+
 class MyStack:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.helper_data_1 = queue.Queue()
+        self.helper_data_2 = queue.Queue()
 
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
+        self.helper_data_2.put(x)
+        while not self.helper_data_1.empty():
+            self.helper_data_2.put(self.helper_data_1.get())
+
+        self.helper_data_1 = self.helper_data_2
+        self.helper_data_2 = queue.Queue()
 
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
+        return self.helper_data_1.get()
 
     def top(self) -> int:
         """
         Get the top element.
         """
+        tmp = self.helper_data_1.get()
+        if tmp:
+            self.push(tmp)
+        return tmp
 
     def empty(self) -> bool:
         """
         Returns whether the stack is empty.
         """
+        return self.helper_data_1.empty()
 
 
 # Your MyStack object will be instantiated and called as such:
@@ -35,3 +51,4 @@ obj.push(4)
 param_2 = obj.pop()
 param_3 = obj.top()
 param_4 = obj.empty()
+print(param_2, param_3, param_4)
--- leetcode/python/p232_MyQueue.py
+++ leetcode/python/p232_MyQueue.py
@@ -4,32 +4,55 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
+        self.stack2.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
+        if len(self.stack1) == 0:
+            while len(self.stack2) > 0:
+                self.stack1.append(self.stack2.pop())
+
+        return self.stack1.pop()
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if len(self.stack1) == 0:
+            while len(self.stack2) > 0:
+                self.stack1.append(self.stack2.pop())
+
+        return self.stack1[-1]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
+        if len(self.stack1) == 0:
+            while len(self.stack2) > 0:
+                self.stack1.append(self.stack2.pop())
+
+        return len(self.stack1) == 0
+
 
 # Your MyQueue object will be instantiated and called as such:
 obj = MyQueue()
-obj.push(100)
-obj.push(101)
-obj.push(102)
+obj.push(1)
+obj.push(2)
+obj.push(3)
+obj.push(4)
+
+param_1 = obj.peek()
 param_2 = obj.pop()
-param_3 = obj.peek()
-param_4 = obj.empty()
+param_3 = obj.empty()
+print(param_1, param_2, param_3)
+
