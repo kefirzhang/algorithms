3d11220a98345cd51285098dc5d90cbff2e348f9
kefirzhang <8390671@qq.com>
2019-10-11 17:02:16 +0800

    nextGreatestLetter

--- leetcode/python/p744_nextGreatestLetter.py
+++ leetcode/python/p744_nextGreatestLetter.py
@@ -1,7 +1,19 @@
 class Solution:
     def nextGreatestLetter(self, letters, target) -> str:
-        return ''
+        if target < letters[0] or target >= letters[-1]:
+            return letters[0]
+        import math
+        start = 0
+        end = len(letters) - 1
+        middle = math.ceil((start + end) / 2)
+        while (letters[middle] > target >= letters[middle - 1]) is False:
+            if letters[middle] > target:
+                end = middle
+            else:
+                start = middle
+            middle = math.ceil((start + end) / 2)
+        return letters[middle]
 
 
 slu = Solution()
-print(slu.nextGreatestLetter(["c", "f", "j"], 'a'))
+print(slu.nextGreatestLetter(["c", "f", "j"], 'c'))
