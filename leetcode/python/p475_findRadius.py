2c82be3feb42fe61a2b4df456675cd91f8f91b3e
kefirzhang <8390671@qq.com>
2019-10-11 23:07:30 +0800

    findRadius

--- leetcode/python/p475_findRadius.py
+++ leetcode/python/p475_findRadius.py
@@ -1,7 +1,7 @@
 import math
-# TODO
 
-class Solution:
+
+class Solution2:
     def findNearBy(self, n, heaters):
         if n <= heaters[0]:
             return heaters[0]
@@ -11,26 +11,63 @@ class Solution:
         length = len(heaters)
         start = 0
         end = length - 1
-        middle = math.ceil((start + end) / 2)
-        while heaters[middle] == n or (
-                heaters[middle - 1] - n < abs(heaters[middle] - n) and abs(heaters[middle] - n) > abs(
-            heaters[middle + 1] - n)):
-            if 1:
-                1
+        middle = math.floor((start + end) / 2)
+        while (heaters[middle] <= n <= heaters[middle + 1]) is False:
+            print(middle, heaters[middle], heaters[middle + 1], n)
+            if heaters[middle] > n:
+                end = middle
+            else:
+                start = middle
+            middle = math.floor((start + end) / 2)
+
+        if n - heaters[middle] < heaters[middle + 1] - n:
+            return heaters[middle]
+        else:
+            return heaters[middle + 1]
 
     def findRadius(self, houses, heaters) -> int:
         heaters.sort()
         helper = []
         for house in houses:
-            distance = False
-            for heater in heaters:
-                if distance is False:
-                    distance = abs(heater - house)
-                else:
-                    distance = min(distance, abs(heater - house))
+            distance = abs(self.findNearBy(house, heaters) - house)
             helper.append(distance)
         return max(helper)
 
 
+class Solution:
+    def findRadius(self, houses, heaters):
+        houses.sort()
+        heaters.sort()
+        len_house = len(houses)
+        len_heaters = len(heaters)
+        i = 0
+        j = 0
+        ans = 0  # 最小距离
+
+        while i < len_house and j < len_heaters:
+            if houses[i] <= heaters[j]:
+                ans = max(ans, heaters[j] - houses[i])
+                i += 1
+            else:
+                if j == len_heaters - 1:
+                    ans = max(ans, houses[i] - heaters[j])
+                    i += 1
+                else:
+                    if heaters[j] < houses[i] <= heaters[j + 1]:
+                        ans = max(ans, min(houses[i] - heaters[j], heaters[j + 1] - houses[i]))
+                        i += 1
+                    else:
+                        j += 1
+
+        return ans
+
+
 slu = Solution()
-print(slu.findRadius([1, 2, 3], [2]))
+print(slu.findRadius([282475249, 622650073, 984943658, 144108930, 470211272, 101027544, 457850878, 458777923],
+                     [823564440, 115438165, 784484492, 74243042, 114807987, 137522503, 441282327, 16531729, 823378840,
+                      143542612]))
+# print(slu.findNearBy(3, [1, 4]))
+'''
+[282475249,622650073,984943658,144108930,470211272,101027544,457850878,458777923]
+[823564440,115438165,784484492,74243042,114807987,137522503,441282327,16531729,823378840,143542612]
+'''
