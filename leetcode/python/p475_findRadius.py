6854b1ee89d566be5cdf486672a817f517eae4d6
kefirzhang <8390671@qq.com>
2019-10-11 19:52:19 +0800

    TODO findNearBy

--- leetcode/python/p475_findRadius.py
+++ leetcode/python/p475_findRadius.py
@@ -1,5 +1,25 @@
+import math
+# TODO
+
 class Solution:
+    def findNearBy(self, n, heaters):
+        if n <= heaters[0]:
+            return heaters[0]
+        elif n >= heaters[-1]:
+            return heaters[-1]
+
+        length = len(heaters)
+        start = 0
+        end = length - 1
+        middle = math.ceil((start + end) / 2)
+        while heaters[middle] == n or (
+                heaters[middle - 1] - n < abs(heaters[middle] - n) and abs(heaters[middle] - n) > abs(
+            heaters[middle + 1] - n)):
+            if 1:
+                1
+
     def findRadius(self, houses, heaters) -> int:
+        heaters.sort()
         helper = []
         for house in houses:
             distance = False
