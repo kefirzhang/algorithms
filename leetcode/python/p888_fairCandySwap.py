ac80f59e7dcbc93e50ea6632e0178844a6869a74
kefirzhang <8390671@qq.com>
2019-10-17 23:03:37 +0800

    fairCandySwap

--- leetcode/python/p888_fairCandySwap.py
+++ leetcode/python/p888_fairCandySwap.py
@@ -2,10 +2,11 @@ class Solution:
     def fairCandySwap(self, A, B):
         sum_a = sum(A)
         sum_b = sum(B)
+        set_b = set(B)
         target = int((sum_a + sum_b) / 2)
         for i in A:
             j = target - (sum_a - i)
-            if j in B:
+            if j in set_b:
                 return [i, j]
 
 
