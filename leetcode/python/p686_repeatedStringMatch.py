b5e0f8af163e19888ea601b21f56768c67f39daf
kefirzhang <8390671@qq.com>
2019-10-20 21:57:05 +0800

    todo

--- leetcode/python/p686_repeatedStringMatch.py
+++ leetcode/python/p686_repeatedStringMatch.py
@@ -1,7 +1,21 @@
 class Solution:
     def repeatedStringMatch(self, A: str, B: str) -> int:
-        return 10
+        if B in A:
+            return 1
+        helper = A
+        min_times = int((len(A)*2 + len(B))/len(A))+1
+        for i in range(1, min_times):
+            helper += A
+            if B in helper:
+                # print(helper)
+                return i + 1
+
+        return -1
 
 
 slu = Solution()
 print(slu.repeatedStringMatch("abcd", "cdabcdab"))
+'''
+"abcd"
+"cdabcdab"
+'''
