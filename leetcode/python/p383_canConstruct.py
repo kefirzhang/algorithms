ceb32c37101bb6895038ce1da6dd3036da2b5ef3
kefirzhang <8390671@qq.com>
2019-10-20 11:09:27 +0800

    str

---
+++ leetcode/python/p383_canConstruct.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for i in ransomNote:
+            if i not in magazine:
+                return False
+            else:
+                magazine.remove(i)
+        return True
+
+slu = Solution()
+print(slu.canConstruct("aa","aab"))
