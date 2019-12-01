551b5ee2fe04936b4bf509eba2fc2322037469e1
kefirzhang <8390671@qq.com>
2019-12-01 15:48:13 +0800

    coinChange

--- leetcode/python/medium/p322_coinChange.py
+++ leetcode/python/medium/p322_coinChange.py
@@ -1,7 +1,23 @@
 class Solution:
     def coinChange(self, coins, amount: int) -> int:
-        return 10
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+        dp = [-1] * (amount + 1)
+        for i in coins:
+            if i <= amount:
+                dp[i] = 1
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i >= coin and dp[i - coin] >= 0:
+                    if dp[i] == -1:
+                        dp[i] = dp[i - coin] + 1
+                    else:
+                        dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[-1]
 
 
 slu = Solution()
-print(slu.coinChange([1, 2, 5], 11))
+print(slu.coinChange([370, 417, 408, 156, 143, 434, 168, 83, 177, 280, 117], 9953))
