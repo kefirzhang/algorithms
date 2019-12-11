00da24de71203488dc12bd269ae6ab9aa8b34929
kefirzhang <8390671@qq.com>
2019-12-11 17:04:00 +0800

    exist

---
+++ leetcode/python/medium/p079_exist.py
@@ -0,0 +1,14 @@
+class Solution:
+    def exist(self, board, word) -> bool:
+
+
+        return True
+
+
+slu = Solution()
+print(slu.exist([
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
+]
+), "ABCCED")
