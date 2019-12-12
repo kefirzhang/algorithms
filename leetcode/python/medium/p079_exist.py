06ffca498c1edf79e2893346e416219e15182154
kefirzhang <8390671@qq.com>
2019-12-12 12:48:42 +0800

    exist

--- leetcode/python/medium/p079_exist.py
+++ leetcode/python/medium/p079_exist.py
@@ -1,14 +1,34 @@
 class Solution:
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    def search(self, board, word, mark, index, row, col, m, n):
+        if index == len(word) - 1:
+            return board[row][col] == word[-1]
+        if word[index] == board[row][col]:
+            mark[row][col] = True
+            for direct in self.directions:
+                new_row = row + direct[0]
+                new_col = col + direct[1]
+                if 0 <= new_row < m and 0 <= new_col < n and mark[new_row][new_col] is False:
+                    if self.search(board, word, mark, index + 1, new_row, new_col, m, n):
+                        return True
+            mark[row][col] = False
+        return False
+
     def exist(self, board, word) -> bool:
+        m = len(board)
+        if m == 0:
+            return False
+        n = len(board[0])
+        mark = [[False for i in range(n)] for j in range(m)]
 
-
-        return True
+        for i in range(m):
+            for j in range(n):
+                if self.search(board, word, mark, 0, i, j, m, n):
+                    return True
+        return False
 
 
 slu = Solution()
-print(slu.exist([
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
-]
-), "ABCCED")
+print(slu.exist([["a", "a", "a", "a"], ["a", "a", "a", "a"], ["a", "a", "a", "a"]],
+                "aaaaaaaaaaaaa"))
