2c7d4354770c4a0d66f182294d8d94adc2edd42b
kefirzhang <8390671@qq.com>
2019-12-18 13:22:23 +0800

    maxCoins

---
+++ leetcode/python/medium/p105_buildTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, preorder, inorder) -> TreeNode:
+        if len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
+
+
+slu = Solution()
+print(slu.buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]))
