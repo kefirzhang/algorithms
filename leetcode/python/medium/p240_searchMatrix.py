b199615b80a6989b1e71e8224db95a0d91ec5db8
kefirzhang <8390671@qq.com>
2019-12-25 16:08:30 +0800

    searchMatrix

---
+++ leetcode/python/medium/p240_searchMatrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        col, row = 0, m - 1
+        while col < n and row >= 0:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:
+                return True
+        return False
+
+
+slu = Solution()
+print(slu.searchMatrix([[-5]], -5))
