c3b5909cf0c625769a1dbbc750f232f06eaf126f
kefirzhang <8390671@qq.com>
2019-12-25 22:26:44 +0800

    rob

--- leetcode/python/medium/p337_rob.py
+++ leetcode/python/medium/p337_rob.py
@@ -7,8 +7,19 @@ class TreeNode:
 
 
 class Solution:
-    def rob(self, root: TreeNode) -> int:
-        return 10
+    def rob(self, root: TreeNode):
+        def helper(node):
+            if not node:
+                return 0, 0
+            if not node.left and not node.right:
+                return node.val, 0
+            left = helper(node.left)
+            right = helper(node.right)
+
+            return node.val + left[1] + right[1], max(left[0] + right[0], left[1] + right[1], left[0] + right[1],
+                                                      left[1] + right[0])
+
+        return max(helper(root))
 
 
 slu = Solution()
@@ -17,3 +28,4 @@ t1.left = TreeNode(2)
 t1.right = TreeNode(3)
 t1.left.right = TreeNode(3)
 t1.right.right = TreeNode(1)
+print(slu.rob(t1))
