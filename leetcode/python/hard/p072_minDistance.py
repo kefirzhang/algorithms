c6207ae43d19ff487269675c511ff322e5c4a25f
kefirzhang <8390671@qq.com>
2019-12-26 10:59:13 +0800

    minDistance

---
+++ leetcode/python/hard/p072_minDistance.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        if m * n == 0:
+            return max(m, n)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            dp[i][0] = i
+        for i in range(1, n + 1):
+            dp[0][i] = i
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                left = dp[i][j - 1] + 1
+                down = dp[i - 1][j] + 1
+                left_down = dp[i - 1][j - 1]
+                if word1[i - 1] != word2[j - 1]:
+                    left_down += 1
+                dp[i][j] = min(left, down, left_down)
+
+        return dp[m][n]
+
+
+slu = Solution()
+print(slu.minDistance("horse", "ros"))
