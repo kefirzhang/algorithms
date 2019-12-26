2631d090290957e55a5b80b79416338821eb13f7
kefirzhang <8390671@qq.com>
2019-12-26 17:36:28 +0800

    canFinish

--- leetcode/python/medium/p207_canFinish.py
+++ leetcode/python/medium/p207_canFinish.py
@@ -1,14 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites) -> bool:
-        helper = {}
-        for i, j in prerequisites:
-            if helper.__contains__(j):
-                helper[j] += 1
-            else:
-                helper[j] = 1
-        return helper
-        return True
+        indegrees = [0 for _ in range(numCourses)]  # 某一个课程依赖多少个
+        adjacency = [[] for _ in range(numCourses)]  # 某一个课程被依赖的课程列表
+        queue = []
+        for cur, pre in prerequisites:
+            indegrees[cur] += 1
+            adjacency[pre].append(cur)
+
+        for i in range(len(indegrees)):
+            if indegrees[i] == 0:
+                queue.append(i)
+
+        while queue:
+            pre = queue.pop()
+            numCourses -= 1
+            for cur in adjacency[pre]:
+                indegrees[cur] -= 1
+                if not indegrees[cur]:
+                    queue.append(cur)
+        return not numCourses
 
 
 slu = Solution()
-print(slu.canFinish(2, [[1, 0]]))
+print(slu.canFinish(2, [[1, 0],[0,1]]))
