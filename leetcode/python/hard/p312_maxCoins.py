56c052c76f00c0cc8d9234e2b683ecc00928cc89
kefirzhang <8390671@qq.com>
2019-12-27 12:57:33 +0800

    maxCoins

---
+++ leetcode/python/hard/p312_maxCoins.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxCoins(self, nums) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for k in range(2, n):
+            for i in range(n - k):
+                j = i + k
+                for t in range(i + 1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][t] + dp[t][j] + nums[i] * nums[t] * nums[j])
+
+        return dp[0][n - 1]
+
+
+slu = Solution()
+print(slu.maxCoins([3, 1, 5, 8]))
