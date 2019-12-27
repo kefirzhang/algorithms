d230ce78b8a0716965f2eddb45366c1b26191517
kefirzhang <8390671@qq.com>
2019-12-27 10:50:24 +0800

    leastInterval

--- leetcode/python/medium/p621_leastInterval.py
+++ leetcode/python/medium/p621_leastInterval.py
@@ -1,7 +1,23 @@
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        return 10
+    def leastInterval(self, tasks, n: int) -> int:
+        helper = {}
+        max_num = 0
+        for i in tasks:
+            if helper.__contains__(i):
+                helper[i] += 1
+            else:
+                helper[i] = 1
+            max_num = max(max_num, helper[i])
+        max_times = 0
+        for i in helper:
+            if helper[i] == max_num:
+                max_times += 1
+
+        min_step = (max_num - 1) * (n + 1) + max_times
+        if min_step < len(tasks):
+            min_step = len(tasks)
+        return min_step
 
 
 slu = Solution()
-print(slu.leastInterval(["A", "A", "A", "B", "B", "B"]))
+print(slu.leastInterval(["A", "A", "A", "B", "B", "B"],2))
