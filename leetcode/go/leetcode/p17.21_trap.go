549e68731d4c05002e35f0499d4f7744f5c63979
kefirzhang <kefirzhang@tencent.com>
2021-04-02 15:37:59 +0800

    trap

--- leetcode/go/leetcode/p17.21_trap.go
+++ leetcode/go/leetcode/p17.21_trap.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package leetcode
 
 func trap(height []int) int {
 	n := len(height)
@@ -45,9 +43,4 @@ func absDiff(a, b int) int {
 	} else {
 		return b
 	}
-}
-
-func main() {
-	s := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
-	fmt.Println(trap(s))
 }
